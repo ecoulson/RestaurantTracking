819120a02527d2bf9f3df0241ab51acdde07ae63
Evan Coulson <evancoulson@Evans-MacBook-Pro-2.local>
2020-05-16 13:48:45 -0700

    Added test for successful registration and registration with empty body

--- server/src/routes/restaurant.js
+++ server/src/routes/restaurant.js
@@ -5,7 +5,16 @@ router.get("/code", (req, res) => {
 });
 
 router.post("/register", (req, res) => {
-    res.send("Hello").status(200)
+    if (Object.keys(req.body).length === 0) {
+        return res.status(400).json({
+            success: false,
+            message: "Failed to register account"
+        })
+    }
+    return res.status(200).json({
+        success: true,
+        message: `Successfully registered ${req.body.name}`
+    })
 });
 
 module.exports = router;
--- server/tests/routes/restaurant.test.js
+++ server/tests/routes/restaurant.test.js
@@ -1,9 +1,31 @@
 const request = require("supertest");
 const app = require("../../src/app");
 
-describe("Restaurant Route Suite", () => {
-    test("A successful registration", async () => {
-        const response = await request(app).post("/restaurant/register");
-        expect(response.statusCode).toBe(200);
-    })
+describe("Restaurant Routes Suite", () => {
+    describe("Registration Route", () => {
+        test("A registration request with empty body", async () => {
+            const response = await request(app).post("/restaurant/register");
+
+            expect(response.status).toBe(400);
+            expect(response.header["content-type"]).toBe("application/json; charset=utf-8");
+            expect(response.body).toEqual({
+                success: false,
+                message: "Failed to register account"
+            });
+        })
+
+        test("A successful registration", async () => {
+            const response = await request(app).post("/restaurant/register").send({
+                number: "4255035202",
+                name: "Bob's Burgers"
+            })
+
+            expect(response.status).toBe(200);
+            expect(response.header["content-type"]).toBe("application/json; charset=utf-8");
+            expect(response.body).toEqual({
+                success: true,
+                message: "Successfully registered Bob's Burgers"
+            });
+        })
+    });
 })
