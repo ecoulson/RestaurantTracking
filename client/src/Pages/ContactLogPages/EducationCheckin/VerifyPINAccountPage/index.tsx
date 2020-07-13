40cb729a1e9043d6b4895550bebdb466811cb6b9
Evan Coulson <evan.m.coulson@gmail.com>
2020-07-13 12:17:45 -0700

    added verify pin email page

--- client/src/Pages/ContactLogPages/EducationCheckin/VerifyPINAccountPage/index.tsx
+++ client/src/Pages/ContactLogPages/EducationCheckin/VerifyPINAccountPage/index.tsx
@@ -4,14 +4,58 @@ import Logo from "../../../../Components/Logo";
 import IVerifyPINAccountPageProps from "./IVerifyPINAccountPageProps";
 import OrganizationName from "../../OrganizationName";
 import IVerifyPINAccountPageState from "./IVerifyPINAccountPageState";
+import GetOrganizationNameRequest from "../../../../API/GetOrganizationNameRequest";
+import IResponse from "../../../../API/IResponse";
+import IGetOrganizationNameResponse from "../../../../API/GetOrganizationNameRequest/IGetOrganizationNameResponse";
+import Form from "../../../../Components/Form";
+import Button from "../../../../Components/Button";
+import Instructions from "../../Instructions";
+import EmailInput from "../../../../Components/EmailInput";
+import IFormValue from "../../../../Components/FormInput/IFormValue";
+import FormValue from "../../../../Components/FormInput/FormValue";
 
 export default class VerifyPINAccountPage extends React.Component<IVerifyPINAccountPageProps, IVerifyPINAccountPageState> {
+    constructor(props : IVerifyPINAccountPageProps) {
+        super(props);
+        this.state = {
+            organizationName: "",
+            send: true,
+            email: new FormValue("", false)
+        }
+        this.onOrganizationName = this.onOrganizationName.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+        this.onEmailChange = this.onEmailChange.bind(this);
+    }
+
     render() {
         return (
             <PageLayout pageTitle="Verify PIN Account">
+                <GetOrganizationNameRequest
+                    organizationId={this.props.match.params.organizationId}
+                    onComplete={this.onOrganizationName}
+                    send />
                 <Logo dark />
-                <OrganizationName></OrganizationName>
+                <OrganizationName>{this.state.organizationName}</OrganizationName>
+                <Form>
+                    <EmailInput dark onChange={this.onEmailChange} iconColor="#707070" hoverColor="white" />
+                    <Instructions>Enter the email associated with your PIN to resend your verification email</Instructions>
+                    <Button submit>Resend</Button>
+                </Form>
             </PageLayout>
         )
+    }
+
+    onOrganizationName(response : IResponse<IGetOrganizationNameResponse>) {
+        this.setState({
+            organizationName: response.data.organizationName
+        })
+    }
+
+    onEmailChange(email: IFormValue<string>) {
+        this.setState({ email })
+    }
+
+    onSubmit() {
+
     }
 }
