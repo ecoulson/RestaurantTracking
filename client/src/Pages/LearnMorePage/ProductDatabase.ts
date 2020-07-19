092f1a437b408e62e100ddd6dab634505603b00d
lauren-west <lwest@hmc.edu>
2020-07-18 23:52:51 -0700

    Update ProductDatabase.ts
    new contact-logs splash imgs and businesses updates

--- client/src/Pages/LearnMorePage/ProductDatabase.ts
+++ client/src/Pages/LearnMorePage/ProductDatabase.ts
@@ -47,7 +47,7 @@ ProductDatabase.set("contact-logs", {
     overview: {
         title: "Returning responsibly during COVID-19",
         about: "Adapt Solutions uses NFC chips, QR codes, and bit.ly links to check-in persons as they enter buildings, allowing colleges and universities to enforce a building capacity, monitor high-risk areas, and identify infected students.",
-        mediaPath: "https://www.hmc.edu/about-hmc/wp-content/uploads/sites/2/2015/01/Shanahan.jpg"
+        mediaPath: "https://images.unsplash.com/photo-1590579491624-f98f36d4c763?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3600&q=80"
     },
     features: [
         {
@@ -69,13 +69,13 @@ ProductDatabase.set("contact-logs", {
     sections: [
         {
             title: "How it works",
-            mediaURL: "https://bestcellphonespyapps.com/wp-content/uploads/2016/03/o-CELL-PHONE-facebook.jpg",
+            mediaURL: "https://images.unsplash.com/photo-1526222344609-ca3e80ea3b06?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=4176&q=80",
             description: "When entering a building, individuals hover, scan, or type a bit.ly link to check-in. If a building is under capacity, persons will reach a success page and be instructed to enter the building. If the building is at capacity, students will receive a notice not to enter."
 
         },
         {
             title: "Smart, data informed decisions",
-            mediaURL: "https://securityintelligence.com/wp-content/uploads/2018/10/si-advanced-authentication-feature.jpg",
+            mediaURL: "https://images.unsplash.com/photo-1550751827-4bd374c3f58b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3450&q=80",
             description: "By analyzing check-in and check-out timestamps, we can notify you of your “chokepoints,” so that you can decrease traffic in high-risk areas. With a simple query, institutions can determine what buildings an infected student has entered. The persons checked-in simultaneously could be encouraged to quarantine for 14 days, and the buildings they entered could be thoroughly sanitized."
             
         }
@@ -89,8 +89,8 @@ ProductDatabase.set("contact-logs", {
 
 ProductDatabase.set("contact-logs-business", {
     productName: "contact-logs-business",
-    name: "Safer-at-business Contact Logs for Offices",
-    description: "Students enter a contact log with the choice of a simple hover, scan, or link. Our data empowers schools to make smart, data informed decisions.",
+    name: "Safer-at-business Contact Logs for Businesses",
+    description: "Employees and Customers enter a contact log with the choice of a simple hover, scan, or link. Our data empowers businesses to make smart, data informed decisions.",
     overview: {
         title: "Returning responsibly during COVID-19",
         about: "Safer-at-School Contact Logs help you to track infected individuals, monitor building capacity, and identify choke points and high risk areas. Check-in and check-out displays at entrances and exits improve your ability to quarantine exposed individuals, reduce high density check-ins by identifying busy checkpoints, and to enforce a building capacity.",
@@ -126,7 +126,7 @@ ProductDatabase.set("contact-logs-business", {
         }
     ],
     pricingModel: {
-        description: "Use our price estimator to calculate your price. Upgrades such as college/university branded QR codes, integration with an identification system, and on-the-go check-in/check-out cards are not included.",
+        description: "Use our price estimator to calculate your price. Upgrades such as branded QR codes, integration with an identification system, and on-the-go check-in/check-out cards are not included.",
         type: PricingModelType.ContactLog,
         pricingStrategy: new ContactLogPricingStrategy()
     }
