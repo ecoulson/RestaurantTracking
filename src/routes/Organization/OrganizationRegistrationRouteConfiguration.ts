68cb8173521f3e16bde90ffb7739e271f351d918
Evan Coulson <evan.m.coulson@gmail.com>
2020-07-20 22:16:33 -0700

    added authorization and authentication to organization registration route

--- src/routes/Organization/OrganizationRegistrationRouteConfiguration.ts
+++ src/routes/Organization/OrganizationRegistrationRouteConfiguration.ts
@@ -3,6 +3,11 @@ import IOrganizationRegistrationController from "../../controllers/Organization/
 import { RegisterOrganizationSchema } from "./OrganizationSchema";
 import ValidationMiddleware from "../../middleware/Validation/ValidationMiddleware";
 import ErrorCatchingMiddleware from "../../middleware/ErrorHandling/ErrorCatchingMiddleware";
+import JSONWebTokenAuthenticationStrategy from "../../middleware/Authentication/JSONWebTokenAuthenticationStrategy";
+import AuthorizationMiddleware from "../../middleware/Authorization/AuthorizationMiddleware";
+import OperationType from "../../lib/Authorization/OperationType";
+import ResourceRequest from "../../lib/Authorization/ResourceRequest";
+import ResourceType from "../../lib/Authorization/ResourceType";
 
 export default class OrganizationRegistrationRouteConfiguration extends RouterConfiguration {
     private controller : IOrganizationRegistrationController;
@@ -15,6 +20,10 @@ export default class OrganizationRegistrationRouteConfiguration extends RouterCo
     configureRoutes() {
         this.router.post(
             '/',
+            new JSONWebTokenAuthenticationStrategy().authenticate(),
+            new AuthorizationMiddleware().authorize(OperationType.Update, async (request) => [
+                new ResourceRequest(request.user._id, ResourceType.User)
+            ]),
             new ValidationMiddleware(RegisterOrganizationSchema).validateBody(),
             ErrorCatchingMiddleware.catchErrors(this.controller.handleRegistration())
         )
