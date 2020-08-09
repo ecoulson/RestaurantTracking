be7ad4adfd3dabcde2976c001dce78c78395a301
Evan Coulson <evan.m.coulson@gmail.com>
2020-08-08 20:54:08 -0700

    add billing plan tests

--- src/services/BillingPlan/GetBillingPlanService.ts
+++ src/services/BillingPlan/GetBillingPlanService.ts
@@ -14,11 +14,14 @@ export default class GetBillingPlanService implements IGetBillingPlanService {
     async getBillingPlan(type : AppType) {
         const plans = await this.billingPlanBroker.getProducts();
         const appPlan = this.getAppPlan(plans.data, type);
+        if (!appPlan) {
+            throw new Error(`No billing plans for ${AppType.ContactLogs}`)
+        }
         const appPlanPrice = await this.billingPlanBroker.getPrices(appPlan.id);
         return appPlanPrice.data;
     }
 
-    getAppPlan(plans : Stripe.Product[], type : AppType) {
+    private getAppPlan(plans : Stripe.Product[], type : AppType) {
         return plans.filter(product => 
             product.metadata.AppType === type && 
             product.metadata.ProductType === ProductType.App)[0]
