8602222e4774e985d037fc5edf58c1e2bf6351bd
Evan Coulson <evan.m.coulson@gmail.com>
2020-08-10 16:55:05 -0700

    added test for organization account verification strategy

--- src/services/Organization/OrganizationAccount/OrganizationAccountVerificationStrategy.ts
+++ src/services/Organization/OrganizationAccount/OrganizationAccountVerificationStrategy.ts
@@ -27,15 +27,21 @@ export default class OrganizationAccountVerificationStrategy implements IVerific
 
     async verify() {
         const user = await this.userBroker.findUserByEmail(this.email);
+        if (!user) {
+            throw new Error(`No user with email ${this.email}`)
+        }
         const tokens = await this.tokenBroker.getTokens(user)
         const verificationTokens = tokens.filter((x) => x.scope.includes(Scope.VerifyEmail));
+        if (verificationTokens.length === 0) {
+            throw new Error(`No verification tokens for user with email: ${this.email}`)
+        }
         const values = await this.tokenService.decryptToken(verificationTokens[0]);
         if (values.get("code") === this.verificationCode) {
             await this.tokenService.deleteExistingToken(user)
             user.verified = true;
             return await user.save();
         } else {
-            throw new Error("Failed to verify user, incorrect verification PIN");
+            throw new Error("Failed to verify user, incorrect verification code");
         }
     }
 }
---
+++ tests/unit/services/Organization/OrganizationAccount/OrganizationAccountVerificationStrategy.test.ts
@@ -0,0 +1,98 @@
+import OrganizationAccountVerificationStrategy from "../../../../../src/services/Organization/OrganizationAccount/OrganizationAccountVerificationStrategy"
+import UserBroker from "../../../../../src/brokers/UserBroker"
+import faker from "faker";
+import { generateObjectId } from "../../../../helpers/mongo";
+import EncryptedTokenService from "../../../../../src/services/Token/EncryptedTokenService";
+import Scope from "../../../../../src/services/Token/Scope";
+import TokenBroker from "../../../../../src/brokers/TokenBroker";
+import UserGenerator from "../../../../mocks/Generators/UserGenerator";
+import TokenGenerator from "../../../../mocks/Generators/TokenGenerator";
+
+const userGenerator = new UserGenerator();
+const tokenGenerator = new TokenGenerator();
+
+describe("Organization Account Verification Strategy", () => {
+    test("No user found with email", async () => {
+        UserBroker.prototype.findUserByEmail = 
+            jest.fn().mockResolvedValue(null);
+        const email = faker.internet.email()
+        const strategy = new OrganizationAccountVerificationStrategy(
+            new UserBroker(),
+            email,
+            generateObjectId(),
+            new EncryptedTokenService(
+                [Scope.VerifyEmail], 1, new TokenBroker()
+            ),
+            new TokenBroker()
+        )
+
+        try {
+            await strategy.verify()
+        } catch (error) {
+            expect(error).toEqual(
+                new Error(`No user with email ${email}`)
+            )
+        }
+
+        expect.assertions(1);
+    })
+
+    test("No verification tokens found for user", async () => {
+        UserBroker.prototype.findUserByEmail = 
+            jest.fn().mockResolvedValue(userGenerator.generate());
+        TokenBroker.prototype.getTokens =
+            jest.fn().mockResolvedValue([]);
+        const email = faker.internet.email()
+        const strategy = new OrganizationAccountVerificationStrategy(
+            new UserBroker(),
+            email,
+            generateObjectId(),
+            new EncryptedTokenService(
+                [Scope.VerifyEmail], 1, new TokenBroker()
+            ),
+            new TokenBroker()
+        )
+
+        try {
+            await strategy.verify()
+        } catch (error) {
+            expect(error).toEqual(
+                new Error(`No verification tokens for user with email: ${email}`)
+            )
+        }
+
+        expect.assertions(1);
+    })
+
+    test("Decrypted token does not have a code in it", async () => {
+        tokenGenerator.setScope([ Scope.VerifyEmail ])
+        UserBroker.prototype.findUserByEmail = 
+            jest.fn().mockResolvedValue(userGenerator.generate());
+        TokenBroker.prototype.getTokens =
+            jest.fn().mockResolvedValue([
+                tokenGenerator.generate()
+            ]);
+        EncryptedTokenService.prototype.decryptToken =
+            jest.fn().mockImplementation(x => new Map())
+        const email = faker.internet.email()
+        const strategy = new OrganizationAccountVerificationStrategy(
+            new UserBroker(),
+            email,
+            generateObjectId(),
+            new EncryptedTokenService(
+                [Scope.VerifyEmail], 1, new TokenBroker()
+            ),
+            new TokenBroker()
+        )
+
+        try {
+            await strategy.verify()
+        } catch (error) {
+            expect(error).toEqual(
+                new Error("Failed to verify user, incorrect verification code")
+            )
+        }
+
+        expect.assertions(1);
+    })
+})
